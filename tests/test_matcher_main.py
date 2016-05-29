5890995acb32d48567857b365035fc218b84bc16
TheCoreMan <dude500@gmail.com>
2016-05-29 19:12:43 +0300

    Added unit tests for the main class of the matcher (I was just testing and testing...)

---
+++ tests/test_matcher_main.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from context import research_tools
+
+
+class TestMatcherMain(TestCase):
+    def test_contains_letters_only(self):
+        self.assertTrue(research_tools.protoobj_matcher.matcher.contains_letters_only("abcdefghijklmnOPQRSTUVWXYz"))
+        self.assertFalse(research_tools.protoobj_matcher.matcher.contains_letters_only("6"))
+        self.assertFalse(research_tools.protoobj_matcher.matcher.contains_letters_only("a word"))
+
+    def test_sanitized_verb(self):
+        self.assertEqual(research_tools.protoobj_matcher.matcher.sanitize_verb("CAPS"), "caps")
+        self.assertRaises(research_tools.protoobj_matcher.matcher.sanitize_verb("a word"))
+        self.assertRaises(research_tools.protoobj_matcher.matcher.sanitize_verb("w0rd"))
