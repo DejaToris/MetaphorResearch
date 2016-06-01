f96ed7b67c35b5bdc6e3062fd86719282f2283b4
Olga Besk <oggiebesk@gmail.com>
2016-06-01 15:38:47 +0300

    Created a data mockup file that will include common objects for verbs, synonyms for verbs, and abstraction values for words.

---
+++ tests/dataMockup.py
@@ -0,0 +1,11 @@
+# contains mock data of the following categories:
+# - common objects for a verb
+# - synonyms for a verb
+# - abstraction values for verbs and nouns
+
+
+verbs_with_synonyms = {'surge': {'arise', 'rise', 'inflate', 'zoom', 'billow'},
+                       'illustrate': {'elaborate', 'show', 'exemplify', 'depict', 'expound'},
+                       'shape': {'determine', 'influence', 'form', 'make', 'cause'},
+                       'mount': {'attach', 'rise', 'bestride', 'wax', 'climb'}}
+
