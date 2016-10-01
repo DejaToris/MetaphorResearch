78199d622cf69fce2ca891163893d58331639518
Olga Besk <oggiebesk@gmail.com>
2016-10-01 22:25:53 +0300

    Extracted POS indices to constants

--- research_tools/COCA.py
+++ research_tools/COCA.py
@@ -3,6 +3,10 @@ import DbAccess as AbstractionDB
 from collections import namedtuple
 import pymssql
 
+POS_NOUN = "1"
+POS_VERB = "2"
+POS_ADJ = "3"
+
 COCAResultTuple = namedtuple('QueryResult', 'run_id freq calcMI SearchCIid SearchPosType FoundCIid FoundPosType FoundWordCI spanLeft spanRight lemma')
 
 def get_common_object_list(verb):
@@ -18,8 +22,10 @@ def get_all_bigrams_for_word(word, spanLeft, spanRight, conn):
     # spans = size of window to look for object.
     # expected spans: Left - 0, Right - 1 or 2 (assuming Det or Adj)
     cursor = conn.cursor()
-    ## cursor.execute("EXEC \"dbo\".\"GetNgrams\" \'{}\', \'2\', \'1\', \'{}\', \'{}\', \'0\'".format(word.lower(), spanLeft, spanRight))
-    cursor.execute("EXEC \"dbo\".\"GetNgrams\" \'{}\', \'3\', \'1\', \'{}\', \'{}\', \'0\'".format(word.lower(), spanLeft, spanRight))
+    ## cursor.execute("EXEC \"dbo\".\"GetNgrams\" \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'0\'".format(word.lower(), POS_VERB, POS_NOUN, spanLeft, spanRight))
+    cursor.execute(
+        "EXEC \"dbo\".\"GetNgrams\" \'{}\', \'{}\', \'{}\', \'{}\', \'{}\', \'0\'".format(word.lower(), POS_ADJ, POS_NOUN,
+                                                                                          spanLeft, spanRight))
     query_result = safe_fetch_all(cursor, word)
     return parse_bigrams(query_result)
 
